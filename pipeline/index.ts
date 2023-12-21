67e484c2d12bd6dd7543500f8cc55e0bdbcdfa5c
Jonathan Yong <yongrenjie@gmail.com>
2023-12-21 00:24:04 +0000

    Update index.ts

--- pipeline/index.ts
+++ pipeline/index.ts
@@ -1,15 +1,5 @@
-/* --------------------------------- */
-/* SCENARIOS                         */
-/* --------------------------------- */
-
-// These are JSON files containing the spatial signature and indicator values
-// for each scenario.
 import baseline from "src/data/scenarios/baseline.json";
 
-// One of the scenarios is used as the 'reference', against which values are
-// scaled.
-const referenceScenarioObject = baseline;
+export const referenceScenarioObject = baseline;
 
-
-// List all the other scenarios here.
-const otherScenarioObjects = []
+export const otherScenarioObjects = []
